c1a611a84a463fb8d45bad623b3ada2190c6ab5d
Pichaya <pichaya.seelapipat@gmail.com>
2018-11-21 13:41:04 +0700

    add print.blade and update adminview

---
+++ resources/views/print.blade.php
@@ -0,0 +1,21 @@
+<h3>พิมพ์สรุป</h3>
+<p>ลูกแก้ว รายชื่อที่จะมาวันนี้ ปุ่มบันทึก(โหลดpdf)</p>
+<input type="button" value="Print" onclick="window.location.href='PDF.php'" />
+<table width="100%" border="1">
+  <thead>
+    <td colspan="1">หมายเลขร้าน</td>
+    <td colspan="1">ชื่อ</td>
+    <td colspan="1">นามสกุล</td>
+    <td colspan="1">ชื่อร้าน</td>
+    <td colspan="1">เบอร์โทรศัพท์</td>
+    <td colspan="1">หมายเหตุ</td>
+  </thead>
+  <tbody>
+    <th>1</th>
+    <th>AAAAAAAAAA</th>
+    <th>BBBBBBBBBB</th>
+    <th>CCCCCCCCCC</th>
+    <th>0800000000</th>
+    <th> </th>
+  </tbody>
+</table>
