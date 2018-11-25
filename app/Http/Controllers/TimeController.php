695107b87c1d20ad9cf7032718ee0b7e8eba557e
Pathomphong Promsit <pathomphong.i@ku.th>
2018-11-25 22:24:05 +0700

    add fn printview TimeController for print page

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -26,6 +26,39 @@ class TimeController extends Controller
 
         return view('userview', ['times' => $times]);
     }
+
+    public function printview()
+    {
+        $times = Time::all();
+
+        return view('printview', ['times' => $times]);
+    }
+
+    public function update($id, Request $request)
+    {
+    	$rules = [
+    		'date' => 'required',
+    		'date1' => 'required',
+    		'person' => 'required',
+    		'name' => 'required'
+    	];
+
+    	$validator = Validator::make($request->all(), $rules);
+
+    	if($validator->fails()) {
+    		return redirect()->back()->withErrors($validator)->withInput();
+    	} else {
+    		$update = Hotel::find($id);
+    		$update->date = $request->input('date');
+    		$update->date1 = $request->input('date1');
+    		$update->name = $request->input('name');
+    		$update->person = $request->input('person');
+            $update->View = 0;
+    		$update->save();
+
+    		return redirect()->route('hotel.index2');
+    	}
+    }
 }
 
 
