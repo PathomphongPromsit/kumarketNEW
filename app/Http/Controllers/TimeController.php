081aec94961c180d0b875aff05ce16b4e6a32151
Pathomphong Promsit <pathomphong.i@ku.th>
2018-11-26 16:47:44 +0700

    new TimeController view userview update

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -2,84 +2,115 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Time;
-use App\User;
 
 class TimeController extends Controller
 {
     /**
-     * Show a list of all of the application's users.
+     * Display a listing of the resource.
      *
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $times = Time::all();
 
-        return view('settime', ['times' => $times]);
+        return view('settime.index', compact('times'));
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $time = Time::find($id);
+
+        return view('settime.edit', compact('time'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'day_ku_night'=>'required',
+            'day_of_week_start'=>'required',
+            'time_start'=>'required',
+            'day_of_week_stop'=>'required',
+            'time_stop'=>'required',
+            'announce'=> 'required',
+        ]);
     
+        $time = Time::find($id);
+
+        $time->day_ku_night = $request->get('day_ku_night');
+        $time->day_of_week_start = $request->get('day_of_week_start');
+        $time->time_start = $request->get('time_start');
+        $time->day_of_week_stop = $request->get('day_of_week_stop');
+        $time->time_stop = $request->get('time_stop');
+        $time->announce = $request->get('announce');
+
+        $time->save();
+
+        return redirect('/settime')->with('success', 'Stock has been updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
     public function userview()
     {
         $times = Time::all();
 
         return view('userview', ['times' => $times]);
     }
-
-    public function printview()
-    {
-        $times = Time::all();
-
-        return view('printview', ['times' => $times]);
-    }
-
-    public function update($id, Request $request)
-    {
-    	$rules = [
-    		'date' => 'required',
-    		'date1' => 'required',
-    		'person' => 'required',
-    		'name' => 'required'
-    	];
-
-    	$validator = Validator::make($request->all(), $rules);
-
-    	if($validator->fails()) {
-    		return redirect()->back()->withErrors($validator)->withInput();
-    	} else {
-    		$update = Hotel::find($id);
-    		$update->date = $request->input('date');
-    		$update->date1 = $request->input('date1');
-    		$update->name = $request->input('name');
-    		$update->person = $request->input('person');
-            $update->View = 0;
-    		$update->save();
-
-    		return redirect()->route('hotel.index2');
-    	}
-    }
-
-    public function SetUncome($id)
-    {
-      $Come = User::find($id);
-      dd($Come);
-      $Come->come = null;
-      $Come->save();
-
-      return redirect('userview');
-    }
-
-    public function Setcome($id)
-    {
-      $Come = User::find($id);
-      $Come->come = 1;
-      $Come->save();
-
-      return redirect('userview');
-    }
-
 }
-
-
