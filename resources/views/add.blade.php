144c7c0197cd6848b6037d8d4c8e84e2100cc3f9
Nopparuj <nopparuj_01@hotmail.com>
2018-11-26 17:39:52 +0700

    Edit add from

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -56,13 +56,13 @@
         </head>
         <body>
             <div id="app">
-                
+
                 <main class="py-4">
                   <div class="container">
                       <div class="row justify-content-center">
                           <div class="col-md-8">
                               <div class="card">
-                                  <div class="card-header">{{ __('Register') }}</div>
+                                  <div class="card-header">{{ __('เพิ่มรายชื่อ') }}</div>
 
                                   <div class="card-body">
                                       <form method="POST" action="{{ route('register') }}">
@@ -97,7 +97,7 @@
                                           </div>
                                           <!-- เพิ่ม -->
                                           <div class="form-group row">
-                                              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('เลขประชาชน') }}</label>
+                                              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('เลขบัตรประจำตัวประชาชน') }}</label>
 
                                               <div class="col-md-6">
                                                   <input id="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -183,7 +183,7 @@
                                           <div class="form-group row mb-0">
                                               <div class="col-md-6 offset-md-4">
                                                   <button type="submit" class="btn btn-primary">
-                                                      {{ __('Register') }}
+                                                      {{ __('เพื่อรายชื่อ') }}
                                                   </button>
                                               </div>
                                           </div>
