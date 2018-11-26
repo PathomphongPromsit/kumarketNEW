4a97b785164218740d170e4d10d26edab7c7799c
Nopparuj <nopparuj_01@hotmail.com>
2018-11-27 00:37:55 +0700

    Edit button add view

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -183,7 +183,7 @@
                                           <div class="form-group row mb-0">
                                               <div class="col-md-6 offset-md-4">
                                                   <button type="submit" class="btn btn-primary">
-                                                      {{ __('เพื่อรายชื่อ') }}
+                                                      {{ __('เพิ่มรายชื่อ') }}
                                                   </button>
                                               </div>
                                           </div>
