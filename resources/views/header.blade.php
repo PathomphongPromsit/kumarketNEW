aeb2f1bb7a00a59b8c1552659d6b2db6cfa4a608
Pathomphong Promsit <pathomphong.i@ku.th>
2018-11-29 21:22:48 +0700

    add logout to header.blade

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -42,35 +42,82 @@ body {font-family: Arial;
 </style>
 </head>
 <body>
-    <div class="ui segment">
-        <div class="ui menu">
-            <div class="header item">
-                <a class="item">
-                    KU SRC Night Market
-                </a>
-                
-                <a class="item" href="settime">
-                    ตั้งเวลา
-                </a>
-            
-                <a class="item" href="print">
-                    พิมพ์รายงาน
-                </a>
-            
-                <a class="item" href="add">
-                    เพิ่มรายชื่อ
-                </a>
-            
-                <a class="item" href="edit">
-                    แก้ไขรายชื่อ
-                </a>
-            
-                <a class="item" href="ban">
-                    รายชื่อผู้ถูกระงับ
-                </a>
+<div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        </div>
+        </nav>
+
+        <main class="py-4">
+            <div class="ui segment">
+                <div class="ui menu">
+                    <div class="header item">
+                        <a class="item">
+                            KU SRC Night Market
+                        </a>
+                        
+                        <a class="item" href="settime">
+                            ตั้งเวลา
+                        </a>
+                    
+                        <a class="item" href="print">
+                            พิมพ์รายงาน
+                        </a>
+                    
+                        <a class="item" href="add">
+                            เพิ่มรายชื่อ
+                        </a>
+                    
+                        <a class="item" href="edit">
+                            แก้ไขรายชื่อ
+                        </a>
+                    
+                        <a class="item" href="ban">
+                            รายชื่อผู้ถูกระงับ
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
-    @yield('content')
 </body>
 </html>
