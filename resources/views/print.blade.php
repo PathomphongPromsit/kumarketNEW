c1a75b6a633ed6cb673d2ef06ceca601bf7b38a7
Pichaya <pichaya.seelapipat@gmail.com>
2018-11-30 09:37:15 +0700

    edit header

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -1,8 +1,22 @@
 <html>
-  <head><title>Print</title><link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" rel="stylesheet">
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+  <title>Print </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
   <style>
   body {font-family: Arial;
@@ -27,29 +41,83 @@
   </head>
   <body>
 
-   <div class="ui segment">
-<div class="ui menu">
-          <div class="header item">
-            <a class="item" href='home'>
-              KU SRC Night Market
-            </a>
-          </div>
-          <a class="item" href='settime'>
-            ตั้งเวลา
-          </a>
-          <a class="item" href='print'>
-            พิมพ์รายงาน
-          </a>
-          <a class="item" href='add'>
-            เพิ่มรายชื่อ
-          </a>
-          <a class="item" href='edit'>
-            แก้ไขรายชื่อ
-          </a>
-          <a class="item" href='ban'>
-            รายชื่อผู้ถูกระงับ
-          </a>
-        </div>
+  <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <div class="ui segment">
+                <div class="ui menu">
+                    <div class="header item">
+                        <a class="item">
+                            KU SRC Night Market
+                        </a>
+                        
+                        <a class="item" href="settime">
+                            ตั้งเวลา
+                        </a>
+                    
+                        <a class="item" href="print">
+                            พิมพ์รายงาน
+                        </a>
+                    
+                        <a class="item" href="add">
+                            เพิ่มรายชื่อ
+                        </a>
+                    
+                        <a class="item" href="edit">
+                            แก้ไขรายชื่อ
+                        </a>
+                    
+                        <a class="item" href="ban">
+                            รายชื่อผู้ถูกระงับ
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 <div div class="card" style="text-align:center">
     <div style="margin:10px;overflow:auto;">
     <h3>พิมพ์สรุป</h3>
