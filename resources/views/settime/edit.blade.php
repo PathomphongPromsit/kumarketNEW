5276e82f4f4851e47bbcc1fbc4253d73e4c6aa4a
Pathomphong Promsit <pathomphong.i@ku.th>
2018-12-11 21:00:15 +0700

    fixed bug default select option

--- resources/views/settime/edit.blade.php
+++ resources/views/settime/edit.blade.php
@@ -36,7 +36,7 @@
         <div class="ui form">
           <div class="inline field">
           <label for="day_ku_night">วันจัดตลาด</label>
-          <select name="day_ku_night" style="width:30%">
+          <!-- <select name="day_ku_night" style="width:30%">
                 <option value="{{$time->day_ku_night}}" selected="selected">{{$time->day_ku_night}}</option>
                 <option value="Sun">อาทิตย์</option>
                 <option value="Mon">จันทร์</option>
@@ -45,12 +45,21 @@
                 <option value="Thu">พฤหัสบดี</option>
                 <option value="Fri">ศุกร์</option>
                 <option value="Sat">เสาร์</option>
-            </select>
+            </select> -->
+            <select name="day_ku_night">
+              <option value="Sun" <?php if ($time->day_ku_night == 'Sun') echo "selected"; ?>>อาทิตย์</option>
+              <option value="Mon" <?php if ($time->day_ku_night == 'Mon') echo "selected"; ?>>จันทร์</option>
+              <option value="Tue" <?php if ($time->day_ku_night == 'Tue') echo "selected"; ?>>อังคาร</option>
+              <option value="Wed" <?php if ($time->day_ku_night == 'Wed') echo "selected"; ?>>พุธ</option>
+              <option value="The" <?php if ($time->day_ku_night == 'The') echo "selected"; ?>>พฤหัสบดี</option>
+              <option value="Fri" <?php if ($time->day_ku_night == 'Fri') echo "selected"; ?>>ศุกร์</option>
+              <option value="Sat" <?php if ($time->day_ku_night == 'Sat') echo "selected"; ?>>เสาร์</option>
+          </select> 
           </div>
 
           <div class="inline field">
           <label for="day_of_week_start">วันเปิดระบบ</label>
-          <select name="day_of_week_start" style="width:30%">
+          <!-- <select name="day_of_week_start" style="width:30%">
                 <option value="{{$time->day_of_week_start}}" selected="selected">{{$time->day_of_week_start}}</option>
                 <option value="Sun">อาทิตย์</option>
                 <option value="Mon">จันทร์</option>
@@ -59,7 +68,16 @@
                 <option value="Thu">พฤหัสบดี</option>
                 <option value="Fri">ศุกร์</option>
                 <option value="Sat">เสาร์</option>
-            </select>
+            </select> -->
+          <select name="day_of_week_start">
+              <option value="Sun" <?php if ($time->day_of_week_start == 'Sun') echo "selected"; ?>>อาทิตย์</option>
+              <option value="Mon" <?php if ($time->day_of_week_start == 'Mon') echo "selected"; ?>>จันทร์</option>
+              <option value="Tue" <?php if ($time->day_of_week_start == 'Tue') echo "selected"; ?>>อังคาร</option>
+              <option value="Wed" <?php if ($time->day_of_week_start == 'Wed') echo "selected"; ?>>พุธ</option>
+              <option value="The" <?php if ($time->day_of_week_start == 'The') echo "selected"; ?>>พฤหัสบดี</option>
+              <option value="Fri" <?php if ($time->day_of_week_start == 'Fri') echo "selected"; ?>>ศุกร์</option>
+              <option value="Sat" <?php if ($time->day_of_week_start == 'Sat') echo "selected"; ?>>เสาร์</option>
+          </select> 
             *ก่อนวันเปิดตลาด 
           </div>
 
@@ -70,7 +88,7 @@
 
             <div class="inline field">
           <label for="day_of_week_stop">วันปิดระบบ</label>
-          <select name="day_of_week_stop" style="width:30%">
+          <!-- <select name="day_of_week_stop" style="width:30%">
                 <option value="{{$time->day_of_week_stop}}" selected="selected">{{$time->day_of_week_stop}}</option>
                 <option value="Sun">อาทิตย์</option>
                 <option value="Mon">จันทร์</option>
@@ -79,7 +97,16 @@
                 <option value="Thu">พฤหัสบดี</option>
                 <option value="Fri">ศุกร์</option>
                 <option value="Sat">เสาร์</option>
-            </select>
+            </select> -->
+            <select name="day_of_week_stop">
+                <option value="Sun" <?php if ($time->day_of_week_stop == 'Sun') echo "selected"; ?>>อาทิตย์</option>
+                <option value="Mon" <?php if ($time->day_of_week_stop == 'Mon') echo "selected"; ?>>จันทร์</option>
+                <option value="Tue" <?php if ($time->day_of_week_stop == 'Tue') echo "selected"; ?>>อังคาร</option>
+                <option value="Wed" <?php if ($time->day_of_week_stop == 'Wed') echo "selected"; ?>>พุธ</option>
+                <option value="The" <?php if ($time->day_of_week_stop == 'The') echo "selected"; ?>>พฤหัสบดี</option>
+                <option value="Fri" <?php if ($time->day_of_week_stop == 'Fri') echo "selected"; ?>>ศุกร์</option>
+                <option value="Sat" <?php if ($time->day_of_week_stop == 'Sat') echo "selected"; ?>>เสาร์</option>
+            </select> 
             *หลังวันเปิดระบบ ไม่เกินวันเปิดตลาด
           </div>
 
@@ -91,8 +118,8 @@
           <label for="announce">ประกาศ</label>
           <input type="text" class="form-control" name="announce" value="{{ $time->announce }}" />
           </div>
-        <td><a href="{{ route('settime.index')}}" class="ui blue button">กลับ</a></td>
         <button type="submit" class="ui green button">บันทึก</button>
+        <td><a href="{{ route('settime.index')}}" class="ui blue button">กลับ</a></td>
 
       </form>
     </div>
