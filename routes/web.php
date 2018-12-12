16c6c3ef16c8cec7812773b57c943153eb9c9bd3
Nuttakit Jaritrum <nuttakit.ja@ku.th>
2018-12-12 20:28:12 +0700

    route
    new route

--- routes/web.php
+++ routes/web.php
@@ -39,10 +39,19 @@ Route::get('editform', function(){  //add
     return view('editform');
 })->middleware('admin');
 
+Route::get('editadmin', function(){  //add
+    return view('edit.editadmin');
+})->middleware('admin');
+
+Route::get('editpassword', function(){  //add
+    return view('edit.editpassword');
+})->middleware('admin');
+
 Route::get('/userview',['as'=>'times','uses'=>'TimeController@userview']);
 
 Route::get('/edit/search', ['as'=>'search','uses' => 'EditPageController@search'])->middleware('admin');
 Route::get('/edit/{id}/del', ['as' => 'del','uses' => 'EditPageController@destroy'])->middleware('admin');
+Route::get('/edit/{id}/editadmin', ['as' => 'editadmin','uses' => 'EditPageController@editAdmin'])->middleware('admin');
 Route::resource('edit', 'EditPageController')->middleware('admin');
 
 Route::get('/ban',['as'=>'ban','uses'=>'BanController@index'])->middleware('admin'); //ban
